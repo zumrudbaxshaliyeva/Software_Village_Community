71c18990bab2bd37e504c60d0362a5eb9d6affa5
zumrudbaxshaliyeva <zumrudbaxsaliyeva@gmail.com>
2024-04-22 10:41:22 +0400

    using api in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import "./App.css"
+const App = () => {
 
-function App() {
+  const [data,setData]=useState([])
+  useEffect(()=>{
+const getData=async ()=>{
+  try{
+    const response=await fetch ('https://api.thecatapi.com/v1/images/search?limit=10')
+    const responseData=await response.json()
+    setData(responseData)
+  }
+catch(error){
+  console.error("Xeta bas verdi",error)
+}
+};
+getData()
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <ul className='list'>
+        {data.map((item,index)=>(
+          <li key={index}>
+            <img src={item.url}/>
+            <h1>{item.id}</h1>
+          </li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
